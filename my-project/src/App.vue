c9b636f63bcb9259b007a40464832c9911827108
silviacremascoli <silcremascoli@gmail.com>
2023-06-27 16:56:00 +0200

    Added initial files

---
+++ my-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+@Options({
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
