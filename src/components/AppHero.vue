45466c3cb04d1f0eebeddd7a1af2818be3ca1599
silviacremascoli <silcremascoli@gmail.com>
2023-07-17 11:27:05 +0200

    Added scrolling event to button

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -2,20 +2,15 @@
   <ParticlesBg />
   <header class="header">
     <h1>Nándor Bottyán</h1>
-    <button
-      v-on:click.prevent="scrollToTop"
-      v-on:scroll="handleScroll"
-      id="scroll-top-button"
-    >
-      <i class="fa-solid fa-arrow-up"></i>
-    </button>
     <h2 class="mb-5">Blockchain and IoT Solution Architect</h2>
     <AppClouds />
+    <AppScrollTopButton />
   </header>
 </template>
 
 <script lang="ts">
 import ParticlesBg from "./ParticlesBg.vue";
+import AppScrollTopButton from "./AppScrollTopButton.vue";
 import AppClouds from "./AppClouds.vue";
 
 export default {
@@ -23,23 +18,7 @@ export default {
   components: {
     ParticlesBg,
     AppClouds,
-  },
-  created() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  unmounted() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    scrollToTop(event: Event) {
-      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    },
-    handleScroll(event: Event) {
-      if (window.scrollY > 20) {
-        const button = document.querySelector("#scroll-top-button");
-        button?.classList.add(".visible");
-      }
-    },
+    AppScrollTopButton,
   },
 };
 </script>
@@ -51,37 +30,6 @@ h1 {
   margin-bottom: 20px;
   font-size: 72px;
   font-family: var(--heading-font);
-}
-
-#scroll-top-button {
-  display: none;
-  position: fixed;
-  right: 20px;
-  bottom: 20px;
-  width: 50px;
-  height: 50px;
-  z-index: 99;
-  border-radius: 50%;
-  background: var(--tertiary-color);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 300ms ease-in-out;
-  transition: transform 0.5s;
-}
-
-.visible {
-  display: block;
-}
-
-#scroll-top-button i {
-  color: var(--secondary-color);
-  font-size: 28px;
-}
-
-#scroll-top-button:hover {
-  transform: scale(1.2);
 }
 
 h2 {
---
+++ src/components/AppScrollTopButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <button
+    v-on:click.prevent="scrollToTop"
+    id="scroll-top-button"
+    :style="{ display: display ? 'block' : 'none' }"
+  >
+    <i class="fa-solid fa-arrow-up"></i>
+  </button>
+</template>
+<script lang="ts">
+export default {
+  name: "AppScrollTopButton",
+  data(): {
+    display: boolean;
+    scrollPosition: number;
+  } {
+    return {
+      display: false,
+      scrollPosition: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+  methods: {
+    scrollToTop(event: Event) {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    },
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+      if (this.scrollPosition > 500) {
+        this.display = true;
+      } else {
+        this.display = false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#scroll-top-button {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  width: 50px;
+  height: 50px;
+  z-index: 99;
+  border-radius: 50%;
+  background: var(--tertiary-color);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+  transition: transform 0.5s;
+}
+
+#scroll-top-button i {
+  color: var(--secondary-color);
+  font-size: 28px;
+}
+
+#scroll-top-button:hover {
+  transform: scale(1.2);
+}
+</style>
